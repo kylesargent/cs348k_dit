bfa73e7b36b6dba2768e5b5ceb7f24c2fd4f8e5a
Eric Ryan Chan <ericryanchan@worldlabs.ai>
2024-06-25 23:38:07 +0000

    update

--- train.py
+++ train.py
@@ -329,7 +329,7 @@ if __name__ == "__main__":
     # Default args here will train DiT-XL/2 with the hyperparameters we used in our paper (except training iters).
     parser = argparse.ArgumentParser()
     parser.add_argument("--data-path", type=str, required=True)
-    parser.add_argument("--results-dir", type=str, default="results")
+    parser.add_argument("--results-dir", type=str, default="benchmarking_results")
     parser.add_argument("--model", type=str, choices=list(DiT_models.keys()), default="DiT-XL/2")
     parser.add_argument("--image-size", type=int, choices=[256, 512], default=256)
     parser.add_argument("--num-classes", type=int, default=1000)
@@ -340,5 +340,7 @@ if __name__ == "__main__":
     parser.add_argument("--num-workers", type=int, default=4)
     parser.add_argument("--log-every", type=int, default=100)
     parser.add_argument("--ckpt-every", type=int, default=50_000)
+    parser.add_argument("--val-every", type=int, default=1000)
     args = parser.parse_args()
+
     main(args)
